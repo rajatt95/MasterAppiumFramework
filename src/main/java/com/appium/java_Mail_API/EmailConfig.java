f0e63b4f6074fda04f7c36a5aa1bdada64ee1c93
“rajatt95” <rajatvermaa95@gmail.com>
2022-04-29 02:55:40 +0530

    Details added in files

--- src/main/java/com/appium/java_Mail_API/EmailConfig.java
+++ src/main/java/com/appium/java_Mail_API/EmailConfig.java
@@ -1,3 +1,15 @@
+/**
+ * @author Rajat Verma
+ * https://www.linkedin.com/in/rajat-v-3b0685128/
+ * https://github.com/rajatt95
+ * https://rajatt95.github.io/
+ *
+ * Course: Appium Mobile Automation - Android & iOS + Frameworks + CICD (https://www.udemy.com/course/the-complete-appium-course-for-ios-and-android/)
+ * Tutor: Omprakash Chavan (https://www.udemy.com/user/omprakash-chavan/)
+ */
+
+/***************************************************/
+
 package com.appium.java_Mail_API;
 
 import static com.appium.constants.FrameworkConstants.Project_Name;
@@ -11,7 +23,7 @@ public class EmailConfig {
 	public static final String PORT = "587";
 
 	public static final String FROM = "testtmail95@gmail.com";
-	public static final String PASSWORD = "HelloSher";
+	public static final String PASSWORD = "********";
 
 	/* "**********@gmail.com", */
 	public static final String[] TO = { "testtmail95@gmail.com" };
